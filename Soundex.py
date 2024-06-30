a6c1204aa9893d1b4884f5010b06df2479fd18cc
Minnu Ajithkumar Latha (MS/ETS1-XC) <ial2kor@bosch.com>
2024-06-30 23:04:56 +0530

    syntax fix

--- Soundex.py
+++ Soundex.py
@@ -10,7 +10,7 @@ def get_soundex_code(c):
     }
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
-def get_code(name,prev_code,soundex)
+def get_code(name,prev_code,soundex):
     for char in name[1:]:
         code = get_soundex_code(char)
         if code != '0' and code != prev_code:
