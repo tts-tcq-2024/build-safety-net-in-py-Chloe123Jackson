88f1a204c1595b34302bd9e58630f73e2b794d49
Minnu Ajithkumar Latha (MS/ETS1-XC) <ial2kor@bosch.com>
2024-06-30 15:24:50 +0530

    Added comments

--- Soundex.test.py
+++ Soundex.test.py
@@ -2,35 +2,35 @@ import unittest
 from Soundex import generate_soundex
 
 class TestSoundex(unittest.TestCase):
-
+    # Empty string
     def test_empty_string(self):
-        self.assertEqual(generate_soundex(""), "")
-
+        self.assertEqual(generate_soundex(""), "")                                          
+    # Single vowel
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
-        
-    def test_single_character(self):
+    # Normal word     
+    def test_normal_word(self):
         self.assertEqual(generate_soundex("Robert"), "R163")
-        
-    def test_single_character(self):
+    # Word with first and second characters sharing the same number     
+    def test_same_group(self):
         self.assertEqual(generate_soundex("Pfister"), "P236")
-    
-    def test_single_character(self):
+    # Word which would result in encoded length being greater than 4
+    def test_wordlength_out_of_range(self):
         self.assertEqual(generate_soundex("INNOCENT"), "I152")
-    
-    def test_single_character(self):
+    # All vowels together
+    def test_all_vowels_together(self):
         self.assertEqual(generate_soundex("aeiou"), "A000")
-    
-    def test_single_character(self):
+    # Word with ending characters separated by a vowel
+    def test_word_separated_by_vowel(self):
         self.assertEqual(generate_soundex("Tymczak"), "T522")
 
-    def test_single_character(self):
+    def test_consonant_vowel_mix(self):
         self.assertEqual(generate_soundex("AebeF"), "A100")
     
-    def test_single_character(self):
+    def test_digits(self):
         self.assertEqual(generate_soundex("0"), "000")
     
-    def test_single_character(self):
+    def test_starts_special_char(self):
         self.assertEqual(generate_soundex("!hello"), "H400")
     
 if __name__ == '__main__':
