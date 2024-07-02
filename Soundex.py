947b8282445996c3017ab15bc070365213e14e65
Minnu Ajithkumar Latha (MS/ETS1-XC) <ial2kor@bosch.com>
2024-07-02 08:36:11 +0530

    Unit test failure check with original code

--- Soundex.py
+++ Soundex.py
@@ -10,18 +10,18 @@ def get_soundex_code(c):
     }
     return mapping.get(c, '0')  # Default to '0' for non-mapped characters
 
-def truncate(soundex):
-    if len(soundex)>4:
-        soundex=soundex[0:3]
-    else:
-        soundex=soundex.ljust(4,'0')
-    return soundex
+# def truncate(soundex):
+#     if len(soundex)>4:
+#         soundex=soundex[0:3]
+#     else:
+#         soundex=soundex.ljust(4,'0')
+#     return soundex
 
-def set_soundex_char(code,prev_code,soundex):
-    if code!='0' and code !=prev_code:
-        soundex += code
-        prev_code = code
-    return soundex,prev_code
+# def set_soundex_char(code,prev_code,soundex):
+#     if code!='0' and code !=prev_code:
+#         soundex += code
+#         prev_code = code
+#     return soundex,prev_code
 
 def generate_soundex(name):
     if not name:
@@ -30,12 +30,20 @@ def generate_soundex(name):
     # Start with the first letter (capitalized)
     soundex = name[0].upper()
     prev_code = get_soundex_code(soundex)
-
     for char in name[1:]:
-        code = get_soundex_code(char)
-        soundex,prev_code=set_soundex_char(code,prev_code,soundex)            
+        code=get_soundex_code(char)
+        if code!=0 and code!=prev_code:
+            soundex+=code
+            prev_code=code
+        if len(soundex)==4:
+            break
+    soundex=soundex.ljust(4,'0')
 
-    soundex=truncate(soundex)
+    # for char in name[1:]:
+    #     code = get_soundex_code(char)
+    #     soundex,prev_code=set_soundex_char(code,prev_code,soundex)            
+
+    # soundex=truncate(soundex)
     # # Pad with zeros if necessary
     # soundex = soundex.ljust(4, '0')
 
