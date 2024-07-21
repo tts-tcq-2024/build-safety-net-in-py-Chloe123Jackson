323eaa18849d8b9a26c36f838706b3362c17bda8
Minnu_A_L <85011295+Chloe123Jackson@users.noreply.github.com>
2024-07-21 19:14:47 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -17,37 +17,12 @@ def truncate(soundex):
         soundex=soundex.ljust(4,'0')
     return soundex
 
-def set_soundex_char(code,prev_code,soundex):
-    if code!='0' and code !=prev_code:
+def set_soundex_char(code,prev_code,soundex,pprev_code):
+    if code!='0' and code !=prev_code and code!=pprev_code and prev_code==0:
         soundex += code
+        pprev_code=prev_code
         prev_code = code
     return soundex,prev_code
-
-# def generate_soundex(name):
-#     if not name:
-#         return ""
-
-#     # Start with the first letter (capitalized)
-#     soundex = name[0].upper()
-#     prev_code = get_soundex_code(soundex)
-#     for char in name[1:]:
-#         code=get_soundex_code(char)
-#         if code!=0 and code!=prev_code:
-#             soundex+=code
-#             prev_code=code
-#         if len(soundex)==4:
-#             break
-#     soundex=soundex.ljust(4,'0')
-
-#     # for char in name[1:]:
-#     #     code = get_soundex_code(char)
-#     #     soundex,prev_code=set_soundex_char(code,prev_code,soundex)            
-
-#     # soundex=truncate(soundex)
-#     # # Pad with zeros if necessary
-#     # soundex = soundex.ljust(4, '0')
-
-#     return soundex
 
 def generate_soundex(name):
     if not name:
@@ -56,9 +31,10 @@ def generate_soundex(name):
     # Start with the first letter (capitalized)
     soundex = name[0].upper()
     prev_code = get_soundex_code(soundex)
+    pprev_code=prev_code
 
     for char in name[1:]:
         code = get_soundex_code(char)
-        soundex,prev_code=set_soundex_char(code,prev_code,soundex)            
+        soundex,prev_code=set_soundex_char(code,prev_code,soundex,pprev_code)            
     soundex=truncate(soundex)
     return soundex
